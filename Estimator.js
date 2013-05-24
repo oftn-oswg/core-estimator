f0570e42694135f2c449af4deb5902f44b6e654a
Devin Samarin <eboyjr14@gmail.com>
2013-05-23 18:22:30 -0700

    Use binary search instead of iterative search to achieve O(log n) running time.

--- Estimator.js
+++ Estimator.js
@@ -1,4 +1,4 @@
-var estimateCores = (function() {
+navigator.getCores = (function() {
 	"use strict";
 
 	function accept(t_score) {
@@ -11,48 +11,36 @@ var estimateCores = (function() {
 		var worker_size = 1;
 		var control;
 
-		loop(function(_repeat) {
+		iterate(function(worker_size, report) {
 
 			measure(workers, worker_size, 5, function(group) {
-
-				var t_score;
 
 				group.analyse();
 
 				if (worker_size === 1) {
 					console.log("1 worker (control group):", group);
 					control = group;
+
+					report(true);
 				} else {
-					t_score = (group.mean - control.mean) /
+					var t_score = (group.mean - control.mean) /
 						Math.sqrt(group.unbiased_variance / group.size + control.unbiased_variance / control.size);
 					console.log(worker_size + " simultaneous workers:", t_score, group);
 
-					if (accept(t_score)) {
-
-						// Not sure why I am doing this
-						// This is a bad thing to do
-						// I guess I am desperate
-						
-						control.add(group.values);
-						control.analyse();
-
-					} else {
-
-						// Terminate our workers, we don't need them anymore.
-						for (var i = 0, len = workers.length; i < len; i++) {
-							workers[i].terminate();
-						}
-
-						// We found an estimate
-						return _continue(worker_size - 1);
-
-					}
-
+					report(accept(t_score));
 				}
-
-				worker_size++;
-				_repeat();
 			});
+
+		}, function(cores) {
+
+			// Terminate our workers, we don't need them anymore.
+			for (var i = 0, len = workers.length; i < len; i++) {
+				workers[i].terminate();
+			}
+
+			// We found an estimate
+			navigator.cores = cores;
+			_continue();
 
 		});
 	}
@@ -97,6 +85,62 @@ var estimateCores = (function() {
 			body(next);
 		}());
 	}
+
+
+	function iterate(test, answer) {
+		// Let S be the set of possible core numbers on this machine.
+		// S = {x \in N | x != 0 }.
+
+		var min, max;
+
+		// Find an upper bound (max - 1) on S by testing powers of two.
+		// During these tests, we also come across a lower bound (min).
+		(function repeat(cores) {
+			test(cores, function(pass) {
+				if (pass) {
+					min = cores;
+
+					// Repeat the test with double the cores.
+					repeat(2 * cores);
+				} else {
+					max = cores;
+
+					// * If S has one element, we found the number
+					// * S has one element iff max - min = 1.
+					// * Given max = min * 2 in invariant of this test,
+					//       S has one element iff min = 1.
+					if (min === 1) {
+						return answer(min);
+					}
+
+					// We have finally found our upper bound; search space.
+					search(min * 3 / 2, min / 4);
+				}
+			});
+		}(1));
+
+		function search(center, pivot) {
+			test(center, function(pass) {
+				if (pass) {
+					min = center;
+					center += pivot;
+				} else {
+					max = center;
+					center -= pivot;
+				}
+				if (max - min === 1)
+					return answer(min);
+
+				if (!pivot) {
+					// This means we haven't found an answer.
+					// Oh well. Answer with the upper bound.
+					return answer(max - 1);
+				}
+				search(center, pivot >> 1);
+			});
+		}
+	}
+
 
 	return estimateCores;
 }());
